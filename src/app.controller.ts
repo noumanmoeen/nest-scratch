233dcdf3d03b0dccde2d4d2dbe365f525c8ef821
moeennouman7@gmail.com <moeennouman7@gmail.com>
2024-04-25 08:06:34 +0500

    Added routing in controller decorators

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,9 +1,13 @@
 import { Controller, Get } from "@nestjs/common";
 
-@Controller()
+@Controller("/app")
 export class AppController {
   @Get()
   getRootRoute() {
     return "Hello";
+  }
+  @Get('/bye')
+  getExitRoute() {
+    return "Bye !!";
   }
 }
